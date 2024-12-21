55b5f866905c3d355765f427bd02127eb1d4a9fe
Amr20533 <amrelnemr638@gmail.com>
2024-12-21 22:30:35 +0200

    Reshaped Architecture

--- lib/core/router/user_get_page_routes.dart
+++ lib/core/router/user_get_page_routes.dart
@@ -3,7 +3,10 @@ import 'package:doctor/view/User/authentication/login.dart';
 import 'package:doctor/view/User/home.dart';
 import 'package:get/get.dart';
 
+import '../../view/User/authentication/welcome.dart';
+
 List<GetPage<dynamic>>? userGetPageRoutes = [
+  GetPage(name: AppRoutes.welcome, page: () => WelcomeScreen()),
   GetPage(name: AppRoutes.userHomePage, page: () => HomeScreen()),
   GetPage(name: AppRoutes.userLogin, page: () => LogInScreen()),
 
--- lib/utils/static/app_routes.dart
+++ lib/utils/static/app_routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes{
   /// <<<<<<<<<<<<<<<<<<<<<<< User Routes >>>>>>>>>>>>>>>>>>>>>
-  static const String userHomePage = '/';
+  static const String welcome = '/';
+  static const String userHomePage = '/user-home';
   static const String userLogin = '/user-login';
 
 
